fb0457774ac0826c46fbc844e03b4acfbf49477d
miketheklim <miketheklim@icloud.com>
2019-06-20 19:46:54 +0200

    update circleci config.yml

--- src/main/java/com/curerny/currency/gateway/BuildInfo.java
+++ src/main/java/com/curerny/currency/gateway/BuildInfo.java
@@ -1,9 +1,11 @@
 package com.curerny.currency.gateway;
 
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
+@Data
 public class BuildInfo {
 
     @Value("${build.number}")
@@ -21,10 +23,8 @@ public class BuildInfo {
     @Value("${circle.workflow.guid}")
     private String workflowGuid;
 
-
     protected static final String CCI_URL_PREFIX ="https://circleci.com/workflow-run/";
     protected static final String GH_URL_FORMAT ="https://github.com/%s/%s/commit/%s";
-
 
     public String getWorkflowUrl() {
         return CCI_URL_PREFIX + workflowGuid;
@@ -38,49 +38,4 @@ public class BuildInfo {
         return String.format(GH_URL_FORMAT, user, repo, commitHash);
     }
 
-
-
-
-    public BuildInfo() {
-    }
-
-    public String getBuildNum() {
-        return buildNum;
-    }
-
-    public void setBuildNum(String buildNum) {
-        this.buildNum = buildNum;
-    }
-
-    public String getCommitHash() {
-        return commitHash;
-    }
-
-    public void setCommitHash(String commitHash) {
-        this.commitHash = commitHash;
-    }
-
-    public String getCommitUser() {
-        return commitUser;
-    }
-
-    public void setCommitUser(String commitUser) {
-        this.commitUser = commitUser;
-    }
-
-    public String getRepoName() {
-        return repoName;
-    }
-
-    public void setRepoName(String repoName) {
-        this.repoName = repoName;
-    }
-
-    public String getWorkflowGuid() {
-        return workflowGuid;
-    }
-
-    public void setWorkflowGuid(String workflowGuid) {
-        this.workflowGuid = workflowGuid;
-    }
 }
